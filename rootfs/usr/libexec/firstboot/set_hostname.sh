0117bb70679c67442905d92d80fd7549d6f59b86
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2024-05-14 11:19:40 -0400

    reference variable

--- rootfs/usr/libexec/firstboot/set_hostname.sh
+++ rootfs/usr/libexec/firstboot/set_hostname.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-prefix=""
-domain=""
+source playbooks.env
 
 if [[ -n "$(dmidecode -s system-serial-number)" ]]
 then
-  new_hostname="${prefix}$(dmidecode -s system-serial-number | tr '[:upper:]' '[:lower:]').${domain}"
+  new_hostname="${PREFIX}$(dmidecode -s system-serial-number | tr '[:upper:]' '[:lower:]').${DOMAIN}"
 else
   address=$(ip a | grep -w inet | tail -n 1 | tr '/' ' ' | awk '{print $2}')
-  new_hostname="${prefix}$(echo ${address} | cut -d. -f3)-$(echo ${address} | cut -d. -f4).${domain}"
+  new_hostname="${PREFIX}$(echo ${address} | cut -d. -f3)-$(echo ${address} | cut -d. -f4).${DOMAIN}"
 fi
 
 hostnamectl set-hostname ${new_hostname}
