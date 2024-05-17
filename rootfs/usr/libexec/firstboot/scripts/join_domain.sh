af7f3156e018e18cdfdb0fba331e860fd423935f
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2024-05-17 13:25:19 +0000

    enable auth

--- rootfs/usr/libexec/firstboot/scripts/join_domain.sh
+++ rootfs/usr/libexec/firstboot/scripts/join_domain.sh
@@ -8,3 +8,6 @@ join_password=$(echo $data | jq -r '.data.data.domain_join_password')
 source /etc/os-release
 echo -n "${join_password}" | adcli join -U ${join_user} --stdin-password --os-name="${NAME}" \
   --os-version="${VERSION}" --os-service-pack="${VERSION_ID}"
+
+systemctl restart sssd
+authselect select sssd with-mkhomedir --force
