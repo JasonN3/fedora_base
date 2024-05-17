e3e4c0d2f0967f88e6696effd0c1537bc13db654
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2024-05-16 21:31:15 -0400

    remove raw

--- rootfs/usr/libexec/firstboot/run_scripts.sh
+++ rootfs/usr/libexec/firstboot/run_scripts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-vault write auth/approle/login \
-  -field=token -format=raw \
+vault write \
+  -field=token \
+  auth/approle/login \
   role_id=$(cat /usr/lib/vault/role_id) \
   secret_id=$(cat /etc/vault/secret_id) > ~/.vault-token
 
