e83c0c66915ad6cf278a31380058ef74de36f185
Jason Nagin <33561705+JasonN3@users.noreply.github.com>
2024-05-20 07:36:49 -0400

    fix var

--- scripts/trigger_builds/trigger_builds.sh
+++ scripts/trigger_builds/trigger_builds.sh
@@ -6,12 +6,12 @@ do
   then
     continue
   fi
-  echo "Triggering ${{ github.repository_owner }}/${repository}"
+  echo "Triggering ${repo_owner}/${repository}"
   curl -L \
     -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${{ secrets.TRIGGER_PAT }}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${{ github.repository_owner }}/${repository}/dispatches \
+    https://api.github.com/repos/${repo_owner}/${repository}/dispatches \
     -d '{"event_type":"rebuild"}'
 done < $(dirname -- $0)/repos
