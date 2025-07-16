c661f624d29ac0946b41662f0182b5c0fb1e217f
Jason N. <33561705+JasonN3@users.noreply.github.com>
2025-07-16 15:39:36 -0400

    remove useless cat

--- rootfs/usr/libexec/pull_images/pull_images.sh
+++ rootfs/usr/libexec/pull_images/pull_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-images=$(cat /var/lib/flightctl/desired.json | jq -r '.spec.applications[].inline[].content' | base64 -d | grep image | awk '{print $2}')
+images=$(jq -r '.spec.applications[].inline[].content' /var/lib/flightctl/desired.json | base64 -d | grep image | awk '{print $2}')
 for image in ${images}
 do
   echo "Checking ${image}"
   if [[ ${image} =~ ^ghcr.io/[Jj]ason[Nn]3/fedora_apps:.* ]]
   then
-    podman pull --decryption-key /etc/pki/fedora_apps.pem ${image}
+    podman pull --decryption-key /etc/pki/fedora_apps.pem "${image}"
   fi
 done
